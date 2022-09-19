b021f32f2ec31da55155fd36ebe3c9517b27bd0b
Simon Waloschek <waloschek@pm.me>
2022-09-19 10:43:08 +0200

    C++ header with compressed HTML binary is now build automatically

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,45 @@
-import { defineConfig } from 'vite'
+import * as fs from 'fs';
+import { defineConfig, normalizePath } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { viteSingleFile } from "vite-plugin-singlefile"
-import viteCompression from 'vite-plugin-compression';
+import chalk from 'chalk';
+import zlib from 'zlib';
 
+function buildHeaderFile() {
+  let config;
+  let inputPath;
+  let outputPath;
+
+  return {
+    name: "build-static-header",
+    apply: "build",
+    enforce: "post",
+
+    configResolved(resolvedConfig) {
+      config = resolvedConfig;
+      const outDir = config.build.outDir;
+      inputPath = normalizePath(outDir + "/index.html");
+      outputPath = normalizePath(outDir + "/index.h");
+    },
+
+    async closeBundle() {
+      const input = fs.readFileSync(inputPath);
+      const compressed = zlib.brotliCompressSync(input);
+      const ratio = compressed.length / input.length;
+      const hexlified = compressed.toString("hex").match(/.{2}/g).map(c => "0x" + c).join(", ");
+      const output = "static const char HTML[] PROGMEM = {" + hexlified + "};";
+      fs.writeFileSync(outputPath, output);
+      config.logger.info(
+        `\n${chalk.green("✓")} Compressed with Brotli.\n${chalk.dim(config.build.outDir + '/')}${chalk.green("index.h")}\t${chalk.dim((compressed.length / 1024).toFixed(2) + " KiB (" + (ratio * 100).toFixed(1) + "%)")}`);
+    }
+  };
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
     viteSingleFile(),
-    viteCompression({
-      algorithm: "brotliCompress",
-    })
+    buildHeaderFile()
   ]
 })
