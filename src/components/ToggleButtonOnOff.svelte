95fafbe3f396be2942dbbc35ec228ede86e6467e
Simon Waloschek <waloschek@pm.me>
2022-10-10 17:59:54 +0200

    Added hover for playmode toggles

--- src/components/ToggleButtonOnOff.svelte
+++ src/components/ToggleButtonOnOff.svelte
@@ -11,13 +11,13 @@
   <div class="cursor-default absolute w-full h-full z-10 bg-zinc-100 opacity-80 rounded overflow-hidden {isDisabled ? 'block' : 'hidden'}"></div>
   <div on:click={() => {if (!isDisabled) isToggled = false}}
        class="flex items-center justify-center w-full h-full rounded-l-md
-              {isToggled ? 'border-l border-y border-zinc-300 bg-zinc-100 uppercase text-zinc-400' : 'border border-orange-500 bg-orange-500 font-bold text-white'}"
+              {isToggled ? 'border-l border-y border-zinc-300 bg-zinc-100 uppercase text-zinc-400 hover:bg-zinc-200' : 'border border-orange-500 bg-orange-500 font-bold text-white'}"
   >
     {$_("common.off")}
   </div>
   <div on:click={() => {if (!isDisabled) isToggled = true}}
        class="flex items-center justify-center w-full h-full rounded-r-md
-              {isToggled ? 'border border-orange-500 bg-orange-500 font-bold text-white' : 'border-r border-y border-zinc-300 bg-zinc-100 uppercase text-zinc-400'}"
+              {isToggled ? 'border border-orange-500 bg-orange-500 font-bold text-white' : 'border-r border-y border-zinc-300 bg-zinc-100 hover:bg-zinc-200 uppercase text-zinc-400'}"
   >
     {$_("common.on")}
   </div>
