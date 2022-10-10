87ba2f9ee7a58aa475de0dd5d2f388bdeddbb008
Simon Waloschek <waloschek@pm.me>
2022-10-10 20:41:23 +0200

    Minor cosmetics

--- src/components/ToggleButtonOnOff.svelte
+++ src/components/ToggleButtonOnOff.svelte
@@ -7,13 +7,12 @@
     $: isToggled = isToggled && !isDisabled;
 </script>
 
-<button class="relative flex flex-row w-full ml-3 w-full tracking-wider uppercase h-full rounded-md overflow-hidden bg-zinc-100 hover:bg-zinc-200 transition duration-150 transition-opacity {isDisabled ? 'opacity-30 cursor-default' : 'opacity-100 cursor-pointer'}"
-        on:click={() => {if (!isDisabled) isToggled = !isToggled}}>
+<button class="relative flex flex-row w-full ml-3 w-full tracking-wider uppercase h-full rounded-md overflow-hidden bg-zinc-100 transition duration-150 transition-opacity
+               {isDisabled ? 'opacity-30 cursor-default' : 'opacity-100 cursor-pointer hover:bg-zinc-200'}" disabled={isDisabled}
+        on:click={() => {if (!isDisabled) isToggled = !isToggled}}
+>
   <div class="absolute w-1/2 h-full bg-orange-500 rounded-md transition duration-150 ease-in-out translate {isToggled ? 'translate-x-[100%]' : ''}"></div>
-  <div class="relative flex items-center justify-center w-full h-full {isToggled && !isDisabled ? 'uppercase text-zinc-400 hover' : 'font-bold text-white'}">
-    {$_("common.off")}
-  </div>
-  <div class="relative flex items-center justify-center w-full h-full {isToggled ? 'font-bold text-white' : 'uppercase text-zinc-400'}">
-    {$_("common.on")}
-  </div>
+
+  <div class="relative flex items-center justify-center w-full h-full transition-all duration-150 ease-in-out {isToggled ? 'text-zinc-400' : 'font-bold text-white'}">{$_("common.off")}</div>
+  <div class="relative flex items-center justify-center w-full h-full transition-all duration-150 ease-in-out {isToggled ? 'font-bold text-white' : 'text-zinc-400'}">{$_("common.on")}</div>
 </button>
