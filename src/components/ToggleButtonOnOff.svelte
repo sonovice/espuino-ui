e0bffc1a0b390e2d8632105ef7878e8559aa599d
Simon Waloschek <waloschek@pm.me>
2022-10-10 20:21:52 +0200

    Simplified playmode toggle

--- src/components/ToggleButtonOnOff.svelte
+++ src/components/ToggleButtonOnOff.svelte
@@ -1,34 +1,19 @@
 <script>
     import {_} from "svelte-i18n";
+
     export let isDisabled = false;
     export let isToggled = false;
 
-    let isHovered = false;
-
     $: isToggled = isToggled && !isDisabled;
-
 </script>
 
-<button class="cursor-default relative flex flex-row w-full ml-3 w-full tracking-wider uppercase h-full rounded-md overflow-hidden
-              {isHovered ? 'bg-zinc-200' : 'bg-zinc-100'}
-              {isDisabled ? 'opacity-30' : 'opacity-100'} transition duration-150 transition-opacity">
-  <div class="absolute w-[50%] h-full bg-orange-500 rounded-md transition duration-150 ease-in-out translate {isToggled ? 'translate-x-[100%]' : ''}"></div>
-  <div class="z-0 flex items-center justify-center w-full h-full
-              {isToggled && !isDisabled ? 'uppercase text-zinc-400 hover' : 'font-bold text-white'}
-              {isHovered ? 'cursor-pointer' : ''}"
-       on:click={() => {if (!isDisabled) isToggled = false; isHovered = false}}
-       on:mousemove={() => isHovered = isToggled && !isDisabled}
-       on:mouseleave={() => isHovered = false}
-  >
+<button class="relative flex flex-row w-full ml-3 w-full tracking-wider uppercase h-full rounded-md overflow-hidden bg-zinc-100 hover:bg-zinc-200 transition duration-150 transition-opacity {isDisabled ? 'opacity-30 cursor-default' : 'opacity-100 cursor-pointer'}"
+        on:click={() => {if (!isDisabled) isToggled = !isToggled}}>
+  <div class="absolute w-1/2 h-full bg-orange-500 rounded-md transition duration-150 ease-in-out translate {isToggled ? 'translate-x-[100%]' : ''}"></div>
+  <div class="relative flex items-center justify-center w-full h-full {isToggled && !isDisabled ? 'uppercase text-zinc-400 hover' : 'font-bold text-white'}">
     {$_("common.off")}
   </div>
-  <div class="z-0 flex items-center justify-center w-full h-full
-              {isToggled ? 'font-bold text-white' : 'uppercase text-zinc-400'}
-              {isHovered ? 'cursor-pointer' : ''}"
-       on:click={() => {if (!isDisabled) isToggled = true; isHovered = false}}
-       on:mousemove={() => isHovered = !isToggled && !isDisabled}
-       on:mouseleave={() => isHovered = false}
-  >
+  <div class="relative flex items-center justify-center w-full h-full {isToggled ? 'font-bold text-white' : 'uppercase text-zinc-400'}">
     {$_("common.on")}
   </div>
 </button>
